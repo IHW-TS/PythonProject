8641705c0656850d4a1af376fdc57724913b2812
Park Tae-Soo <78849961+IHW-TS@users.noreply.github.com>
2023-02-15 15:23:51 +0100

    vf

--- Test/tete.py
+++ Test/tete.py
@@ -1,83 +1,168 @@
-# "copy" permet de copier des objets en Python sans les lier à leur source.
+#!/bin/python
+#-*-coding:utf-8-*-
+ 
 import copy
-
-n = int(input("Entrez le nombre de missionnaires et de cannibales: "))
-p = int(input("Entrez la capacité de la bateau : "))
-
-start = [n, n, 1]
-size = n
-possible_actions = []
-
-for i in range(p + 1):
-    for j in range(p - i + 1):
-        if i + j <= p:
-            possible_actions.append([i, j, 1])
-
-memo = []
-solution = 0
-
-def suppr(vec1, vec2):
-    return [vec1[i] - vec2[i] for i in range(3)]
-
-def add(vec1, vec2):
-    return [vec1[i] + vec2[i] for i in range(3)]
-
-def v_add(vec):
-    actions = [] 
-    for action in possible_actions:
-        if action[0] + action[1] >= 1:
-            x = add(vec, action) 
-            y = suppr([size, size, 1], x) 
-            if x[0] >= 0 and x[1] >= 0 and x[0] <= size and x[1] <= size and (x[0] >= x[1] or x[0] == 0) and (y[0] >= y[1] or y[0] == 0):
-                actions.append(action)        
-    return actions 
-
-def v_suppr(vec):
-    actions = []
-    for action in possible_actions:
-        if action[0] + action[1] >= 1: 
-            x = suppr(vec, action)
-            y = suppr([size, size, 1], x)
-            if x[0] >= 0 and x[1] >= 0 and x[0] <= size and x[1] <= size and (x[0] >= x[1] or x[0] == 0) and (y[0] >= y[1] or y[0] == 0):
-                actions.append(action)
-    return actions
-
-def tree_search(vec, moves):
-    global solution
-
-    if vec in memo:
-        return
-    else:
-        memo.append(vec)
-
-    moves = copy.deepcopy(moves)
-    moves += 1
-
-    if vec[2] == 0: 
-        actions = v_add(vec)
-    else:
-        actions = v_suppr(vec)
-
-    for action in actions:
-        new_vec = copy.deepcopy(vec)
-        for i in range(3):
-            if new_vec[2] == 0:
-                new_vec[i] += action[i]
-            else:
-                new_vec[i] -= action[i]
-        if sum(new_vec) == 0:
-            print("Etape Finale", moves, "=", [new_vec[0], new_vec[1], new_vec[2], size-new_vec[0], size-new_vec[1]])
-            solution = moves
-            return True
-        if tree_search(new_vec, moves):
-            print("Etape",moves, "=", [new_vec[0], new_vec[1], new_vec[2], size-new_vec[0], size-new_vec[1]])
-            return True
-    return False
-
-tree_search(start, 0)
-
-print("Le nombre d'etape totale est de =", solution)
-
-
-
-
+ 
+# donnees des noeuds
+F, G, H, JEU, PAPA = 0, 1, 2, 3, 4
+ 
+class Solveur :
+    '''
+    Le solveur de taquin
+    '''
+ 
+    def __init__(self, taquin) :
+        self.openList = []
+        self.closedList = []
+ 
+        self.taquinInitial = taquin
+        self.numeroCaseVide = taquin.numeroCaseVide
+        self.taille = len(taquin.jeu)
+ 
+        # on met l'etat initial
+        self.openList.append([0, 0, 0, taquin.jeu, None])
+ 
+ 
+    def resoudre(self) :
+        print "Recherche de la solution..."
+        while len(self.openList) > 0 :
+ 
+            noeudCourant = self.openList.pop(0)
+            taquin = noeudCourant[JEU]
+ 
+            #self.closedList.append()
+ 
+            if noeudCourant[JEU] == self.taquinInitial.jeuTrie :
+                return noeudCourant
+ 
+            # ajout des possibilites ...
+            for ligne in range(0, self.taille) :
+                for colonne in range(0, self.taille) :
+ 
+                    if colonne > 0 and taquin[ligne][colonne-1] == self.numeroCaseVide :
+                        tq = copy.deepcopy(taquin)
+                        tq[ligne][colonne], tq[ligne][colonne-1] = tq[ligne][colonne-1], tq[ligne][colonne]
+                        g = noeudCourant<strong>+1
+                        h = self.heuristique(tq)
+                        if not tq in self.closedList :
+                            i = self.indiceDansListeOuverte(tq)
+                            if i == -1 :
+                                self.openList.append([g+h, g, h, tq, noeudCourant])
+                            elif g+h < self.openList<em>[F] :
+                                self.openList[i] = [g+h, g, h, tq, noeudCourant]
+ 
+                    if colonne < self.taille - 1 and taquin[ligne][colonne+1] == self.numeroCaseVide :
+                        tq = copy.deepcopy(taquin)
+                        tq[ligne][colonne], tq[ligne][colonne+1] = tq[ligne][colonne+1], tq[ligne][colonne]
+                        g = noeudCourant[G]+1
+                        h = self.heuristique(tq)
+                        if not tq in self.closedList :
+                            i = self.indiceDansListeOuverte(tq)
+                            if i == -1 :
+                                self.openList.append([g+h, g, h, tq, noeudCourant])
+                            elif g+h < self.openList[i][F] :
+                                self.openList[i] = [g+h, g, h, tq, noeudCourant]
+ 
+                    if ligne > 0 and taquin[ligne-1][colonne] == self.numeroCaseVide :
+                        tq = copy.deepcopy(taquin)
+                        tq[ligne][colonne], tq[ligne-1][colonne] = tq[ligne-1][colonne], tq[ligne][colonne]
+                        g = noeudCourant[G]+1
+                        h = self.heuristique(tq)
+                        if not tq in self.closedList :
+                            i = self.indiceDansListeOuverte(tq)
+                            if i == -1 :
+                                self.openList.append([g+h, g, h, tq, noeudCourant])
+                            elif g+h < self.openList[i][F] :
+                                self.openList[i] = [g+h, g, h, tq, noeudCourant]
+ 
+                    if ligne < self.taille - 1 and taquin[ligne+1][colonne] == self.numeroCaseVide :
+                        tq = copy.deepcopy(taquin)
+                        tq[ligne][colonne], tq[ligne+1][colonne] = tq[ligne+1][colonne], tq[ligne][colonne]
+                        g = noeudCourant[G]+1
+                        h = self.heuristique(tq)
+                        if not tq in self.closedList :
+                            i = self.indiceDansListeOuverte(tq)
+                            if i == -1 :
+                                self.openList.append([g+h, g, h, tq, noeudCourant])
+                            elif g+h < self.openList[i][F] :
+                                self.openList[i] = [g+h, g, h, tq, noeudCourant]
+ 
+            self.openList.sort()
+ 
+        print None
+ 
+ 
+    def indiceDansListeOuverte(self, jeu) :
+        for i in range(0, len(self.openList)) :
+            if self.openList[i][3] == jeu :
+                return i
+        return -1
+ 
+ 
+    def heuristique(self, taquin) :
+        i = 0
+        for ligne in range(0, self.taille) :
+            for colonne in range(0, self.taille) :
+                if taquin[ligne][colonne] != self.taquinInitial.jeuTrie[ligne][colonne] :
+                    i += 1
+        return i
+ 
+ 
+class Taquin :
+    '''
+    Le jeu en lui meme
+    '''
+ 
+    def __init__(self, jeu = None) :
+        if jeu != None :
+            self.setJeu(jeu)
+ 
+    def __repr__(self) :
+        return 'n'.join( ''.join(str(i)) for i in self.jeu )
+ 
+    def setJeu(self, jeu) :
+        self.jeu = [ list(i) for i in jeu ]
+        self.numeroCaseVide = len(self.jeu) - 1
+ 
+        self.jeuTrie = [ [] for i in range(len(self.jeu)) ] # pour ne pas le recalculer a chaque fois
+        for i in range(len(self.jeu)) :
+            self.jeuTrie[i] = [ j for j in range(len(self.jeu)*i, len(self.jeu)*i + len(self.jeu)) ]
+ 
+    def setNumeroCaseVide(self, i) :
+        self.numeroCaseVide = i
+ 
+    def printEtatFini(self) :
+        print 'n' + 'n'.join( ''.join(str(i)) for i in self.jeuTrie )
+ 
+    def termine(self) :
+        return [ i for i in self.jeu ] == self.jeuTrie
+ 
+    def resoudre(self) :
+        solveur = Solveur(self)
+        res = solveur.resoudre()
+        if res == None :
+            print "Pas de solution trouvee"
+            return
+        print "Solution trouvee"
+ 
+        L = []
+        while res[PAPA] != None :
+            L.append(res[PAPA][JEU])
+            res = res[PAPA]
+        L.reverse()
+ 
+        for elem in L :
+            print 'n' + 'n'.join( ''.join(str(i)) for i in elem )
+        self.printEtatFini()
+ 
+'''
+6 3 1
+8 0 2
+7 4 5
+'''
+ 
+if __name__ == "__main__" :
+    tq = Taquin()
+    tq.setJeu( [ [6, 3, 1], [8, 0, 2], [7, 4, 5] ] )
+    tq.setNumeroCaseVide(2)
+    tq.resoudre()
